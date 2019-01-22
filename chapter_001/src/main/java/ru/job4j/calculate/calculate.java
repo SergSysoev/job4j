0cff7588f536b3f8f58f8fb1aa6f7e6ee4ccfc43
SergSysoev <p2533155552@gmail.com>
2019-01-21 21:10:38 -0800

    1.3. JavDoc. Code convetion.

--- chapter_001/src/main/java/ru/job4j/calculate/calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/calculate.java
@@ -1,6 +1,24 @@
 package ru.job4j.calculate;
 
+/**
+
+*Package for calculate task.
+*
+*@author Serg Sysoev;
+*@since 01/21/2019;
+*@version $Id$.
+
+*/
+
 public class calculate {
+/**
+
+*Method main.
+*
+*@param args
+*@return "Hello World!"
+
+*/
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
